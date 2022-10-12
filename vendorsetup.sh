4870747deeac6d2694646f4b5c9852c480ce3851
Powerfull11 <110167786+Powerfull11@users.noreply.github.com>
2022-10-12 13:30:06 +0530

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,13 +18,14 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="RMX2117"
+#set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
    if [ -n "$chkdev" ]; then
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -34,80 +35,72 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
+	# Configure some default settings for the build
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+	export OF_VANILLA_BUILD=0
+	export OF_SKIP_ORANGEFOX_PROCESS=1
+	export OF_SUPPORT_OZIP_DECRYPTION=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export TARGET_DEVICE_ALT="shiva"
+	export LC_ALL="C"
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_PATCH_AVB20=1
+        export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+
+	# Some device specific flags
+	export OF_STATUS_H="102"
+	export OF_STATUS_INDENT_LEFT="155"
+	export OF_STATUS_INDENT_RIGHT="48"
+	export OF_SCREEN_H="2400"
+	export OF_TWRP_COMPATIBILITY_MODE=1
+	export OF_FL_PATH1="/system/flashlight"
+	export OF_FL_PATH2=""
+	export OF_FLASHLIGHT_ENABLE=1
+	export OF_MAINTAINER="Powerfull11"
+	export OF_TARGET_DEVICES="RMX2117, RMX2111, RMX2117L1, RMX2111CN, RMX2117IN"
+	export TARGET_DEVICE_ALT="RMX2117, RMX2111"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
 
-	# Screen Settings
-	export OF_SCREEN_H=2340
-	export OF_STATUS_INDENT_LEFT="48"
-	export OF_STATUS_INDENT_RIGHT="48"
-	export OF_ALLOW_DISABLE_NAVBAR="0"
-
+	# Some Extra Vars
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_GREP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_TAR_BINARY=1
+	export OF_QUICK_BACKUP_LIST="/super;/boot;/dtbo;/vbmeta;/vbmeta_system;/vbmeta_vendor;/data"
+	export OF_USE_TWRP_SAR_DETECT=1
+	export OF_CHECK_OVERWRITE_ATTEMPTS=1
+	export OF_HIDE_NOTCH=1
+	export OF_USE_GREEN_LED=0
+	export OF_SUPPORT_PRE_FLASH_SCRIPT=1
 	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-
-	# OTA
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
-
-
-	# R11.1 Setings
-	export FOX_BUILD_TYPE="Unofficial"
-	export OF_MAINTAINER="@Powerfull11SK"
-	export FOX_VERSION="R11.1_2-@Powerfull11"
-
+	export OF_ALLOW_DISABLE_NAVBAR=0
 	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+
+	#R11
 	export FOX_R11=1
-	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
-	export OF_FLASHLIGHT_ENABLE=0
-	export FOX_DELETE_AROMAFM=1
-	export OF_PATCH_AVB20=1
-	export OF_FBE_METADATA_MOUNT_IGNORE="1"
-	export FOX_REPLACE_BUSYBOX_PS="0"
-	export OF_CHECK_OVERWRITE_ATTEMPTS=1
-	export OF_USE_SYSTEM_FINGERPRINT=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_USE_LOCKSCREEN_BUTTON=1
-	export FOX_ENABLE_APP_MANAGER=0
-	export FOX_VARIANT=MIUI
-	export FOX_DELETE_INITD_ADDON=1
-	export OF_SKIP_ORANGEFOX_PROCESS=1
-	# Run a Process After Formatting Data to Work-Around MTP Issues
+	export FOX_VERSION="R11.1_1"
+	export FOX_BUILD_TYPE="Stable"
+	export FOX_VARIANT="RUI2.0"
+
+	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
+	export OF_SPLASH_MAX_SIZE="104"
+
+	# run a process after formatting data to work-around MTP issues
 	export OF_RUN_POST_FORMAT_PROCESS=1
-        export FOX_DELETE_MAGISK_ADDON=1
-        
-        #skip fbe
-        export OF_SKIP_FBE_DECRYPTION=1
-        export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
-        export OF_FBE_METADATA_MOUNT_IGNORE=1
-        export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+
+	# Custom pic for the maintainer's info in about section
+	export OF_MAINTAINER_AVATAR="$PWD/device/realme/RMX2117/maintainer.png"
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
+	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+	  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	fi
 fi
-#
